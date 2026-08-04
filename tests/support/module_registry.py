dca19b69c1cfb6373ee17d1abfc8465e6ea9160d
Klaudio Sinani <klaudiosinani@protonmail.ch>
2026-08-04 17:49:31 +0200

    Set `package` method return type to `Any`

--- tests/support/module_registry.py
+++ tests/support/module_registry.py
@@ -1,8 +1,9 @@
 import sys
 import types
+from typing import Any
 
 
-def package(name: str) -> types.ModuleType:
+def package(name: str) -> Any:
     module = types.ModuleType(name)
     module.__path__ = []
     return module
