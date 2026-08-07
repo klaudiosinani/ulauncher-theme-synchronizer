b4ca7182f6c416e0d86ea4be0372a9a6c2e76e29
Klaudio Sinani <klaudiosinani@protonmail.ch>
2026-08-08 01:17:53 +0200

    Remove unused global constant from theme parser module

--- src/feature/synchronization/theme/theme_parser.py
+++ src/feature/synchronization/theme/theme_parser.py
@@ -1,8 +1,6 @@
 from src.feature.synchronization.mode.mode import Mode
 from src.feature.synchronization.theme.theme import Theme
 
-DARK_KEYWORD = "dark"
-
 
 class ThemeParser:
     def parse(self, mode: Mode) -> Theme:
